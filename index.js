178e0c4c2b0fb1b3c6daef699653f164bc0b0d2f
Amy Zhou <aymeighz@gmail.com>
2018-07-16 21:12:04 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,9 +10,8 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value) {
 }
 
 function deleteFromObjectByKey(object, key) {
-  var newObj = Object.assign({}, object);
-  delete newObj[key];
-  return newObj;
+  delete object.key;
+  return object;
 }
 
 function destructivelyDeleteFromObjectByKey(object, key) {
